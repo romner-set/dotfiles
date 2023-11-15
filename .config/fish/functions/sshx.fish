6c82705c3b03396e396438533d47fa84f3e477f2
romner-set <roman@skotnica.com>
2023-11-15 19:57:30 +0100

    stdnyetdsnye

--- .config/fish/functions/sshx.fish
+++ .config/fish/functions/sshx.fish
@@ -3,5 +3,5 @@ function sshx
   if test (count $argv) -gt 1
     set -f ssh_args $argv[2..(count $argv)]
   end
-  ssh (cat "$sp/user")@(oci_get_ip "$argv[1]" | head -1) -p(cat "$sp/port") -i "$sp/id_ed25519" -o StrictHostKeyChecking=no $ssh_args
+  ssh (cat "$sp/user")@(oci_get_ip "$argv[1]" | head -1) -p(cat "$sp/port") -i "$sp/id_ed25519" $ssh_args
 end
